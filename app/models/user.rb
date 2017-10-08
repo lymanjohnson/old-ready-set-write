5f567b25d80d0b3b5d9b0f07401ed1f1f050a58e
lymanjohnson <lyman.johnson@gmail.com>
2017-10-08 12:27:37 -0400

    Removes problematic migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,4 @@
 class User < ApplicationRecord
-  # has_many :user_cards
   has_many :user_games
-  # has_many :user_pages
-  # has_many :user_traits
-  # has_many :pages, :through => :user_pages
   has_many :games, :through => :user_games
-  # has_many :cards, :through => :user_cards
-  # has_many :traits, :through => :user_traits
 end
