607f0b86e4f77d142d2a0a7784c429a0017bbf22
lymanjohnson <lyman.johnson@gmail.com>
2017-10-08 14:11:03 -0400

    Removed inter-card relationships for now. Needs more complex model.

--- app/models/user_card.rb
+++ app/models/user_card.rb
@@ -1,10 +1,5 @@
 class UserCard < ApplicationRecord
-  # belongs_to :user
   belongs_to :card
   belongs_to :user_game
-  has_many :relationships
-  has_and_belongs_to_many :user_cards, :through => :relationships
   has_many :user_traits, dependent: :destroy
-
-
 end
