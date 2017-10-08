674ee0c568b8c62b0aa2709b1a9b5668240b7833
lymanjohnson <lyman.johnson@gmail.com>
2017-10-08 13:34:47 -0400

    Adds inter-card relationships. Unclear if it is set up properly.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008154230) do
+ActiveRecord::Schema.define(version: 20171008171839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20171008154230) do
   end
 
   create_table "pages", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
